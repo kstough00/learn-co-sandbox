72664e90db5c58a16da479c712828a9ae1c1c475
Kyle Stough <kstough00@gmail.com>
2021-02-17 17:41:38 +0000

    Automatically backed up by Learn

---
+++ complex method call.rb
@@ -0,0 +1,20 @@
+def greeting
+  puts "Hi, Ruby programmer!"
+end
+
+def greeting_python
+  puts "Hello, Python programmer!"
+end
+
+def greeting(name)
+  puts "Hello, #{name}!"
+end
+
+def greeting_a_person(name)
+  "Hello #{name}"
+end
+
+def greeting_programmer(name, language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+
